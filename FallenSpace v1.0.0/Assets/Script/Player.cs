0a23e8dbbc5d9425674c2fb3aa7c43b720490775
SHARAF <porvaredd@gmail.com>
2018-04-29 12:00:32 +0700

    jump not yet

--- FallenSpace v1.0.0/Assets/Script/Player.cs
+++ FallenSpace v1.0.0/Assets/Script/Player.cs
@@ -13,6 +13,17 @@ public class Player : MonoBehaviour {
 	private bool facingRight;
 	private bool attack;
 
+	[SerializeField]
+	private Transform[] groundPoints;
+	private float groundRadius;
+
+	private LayerMask whatIsGround;
+
+	private bool isGrounded;
+
+	private bool jump;
+	[SerializeField]
+	private float jumpForce;
 	// Use this for initialization
 	void Start () {
 		
@@ -29,6 +40,9 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float horizontal = Input.GetAxis ("Horizontal");
+
+		isGrounded = IsGrounded ();
+
 		HandleMovement (horizontal);
 		Flip (horizontal);
 		HandleAttacks ();
@@ -39,7 +53,11 @@ public class Player : MonoBehaviour {
 		if(!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")){
 			myRigidbody.velocity = new Vector2 (Horizontal * movementSpeed, myRigidbody.velocity.y);
 		}
-			
+		if (isGrounded && jump) {
+			isGrounded = false;
+			myRigidbody.AddForce (new Vector2 (0, jumpForce));
+		}
+
 		myAnimator.SetFloat ("speed", Mathf.Abs(Horizontal));
 		print (Horizontal);
 	}
@@ -52,6 +70,9 @@ public class Player : MonoBehaviour {
 	}
 
 	private void HandleInput(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			jump = true;
+		}
 		if (Input.GetKeyDown (KeyCode.LeftShift)) {
 			attack = true;
 		}
@@ -72,5 +93,24 @@ public class Player : MonoBehaviour {
 
 	private void resetValues(){
 		attack = false;
+		jump = false;
+
 	}
+	private bool IsGrounded(){
+		if(myRigidbody.velocity.y <= 0){
+			foreach (Transform point in groundPoints){
+				Collider2D[] colliders = Physics2D.OverlapCircleAll (point.position, groundRadius, whatIsGround);
+
+				for (int i = 0; i < colliders.Length; i++) {
+					if(colliders[i].gameObject != gameObject) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+
+
 }
