bed769b38f96ac8e70097b4c4a1f47f702659508
SHARAF <porvaredd@gmail.com>
2018-05-01 00:55:23 +0700

    Connect to db level
    add script
    - healthbar.cs
    -monter.cs

---
+++ FallenSpace v1.0.0/Assets/Script/Healthbar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+	
+
+	Image HealthBar;
+	//float maxHealth = 100f;
+	public static float health;
+	float  Text_get_hp_f;
+	public GameObject spaceMan;
+
+	public Text Text_get_hp;
+
+	// Use this for initialization
+	void Start () {
+		
+		string Text_get_hp = PlayerPrefs.GetString ("hp");
+		Text_get_hp_f = float.Parse(Text_get_hp);
+
+		HealthBar = GetComponent<Image> ();
+		//health = maxHealth;
+		health = Text_get_hp_f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		HealthBar.fillAmount = health / Text_get_hp_f;
+		Debug.Log (health);
+
+		Text_get_hp.text = health.ToString();
+
+		if(health <= 0){
+			Destroy (spaceMan, 5);
+
+		}
+	}
+}
--- FallenSpace v1.0.0/Assets/Script/LoginOutput.cs
+++ FallenSpace v1.0.0/Assets/Script/LoginOutput.cs
@@ -5,7 +5,16 @@ using UnityEngine.UI;
 
 public class LoginOutput : MonoBehaviour {
 
-	public Text Text_get;
+	public Text Text_get_username;
+	public Text Text_get_email;
+	public Text Text_get_password;
+	public Text Text_get_name;
+	public Text Text_get_money;
+	public Text Text_get_score;
+	public Text Text_get_state;
+	public Text Text_get_level;
+	public Text Text_get_exp;
+	public Text Text_get_hp;
 
 
 	// Use this for initialization
@@ -16,8 +25,21 @@ public class LoginOutput : MonoBehaviour {
 		Creature jsonuser = JsonUtility.FromJson<Creature> (user_name);
 		Debug.Log (jsonuser.id);
 		Debug.Log (jsonuser.username);
+		Debug.Log (jsonuser.hp);
 
-		Text_get.text = "HELLO " + jsonuser.username;
+
+		Text_get_username.text = jsonuser.username;
+		Text_get_email.text = jsonuser.email;
+		Text_get_password.text = jsonuser.password;
+		Text_get_name.text = jsonuser.name;
+		Text_get_money.text = jsonuser.money;
+		Text_get_score.text = jsonuser.score;
+		Text_get_state.text = jsonuser.state;
+		Text_get_level.text = jsonuser.level;
+		Text_get_exp.text = jsonuser.exp;
+		Text_get_hp.text = jsonuser.hp;
+
+		PlayerPrefs.SetString ("hp", jsonuser.hp); //ส่งค่า
 	}
 }
 	
@@ -26,5 +48,13 @@ public class Creature {
 	public int id;
 	public string username;
 	public string email;
+	public string password;
+	public string name;
+	public string money;
+	public string score;
+	public string state;
+	public string level;
+	public string exp;
+	public string hp;
 }
 
