3e54d1123f4ecd255b95edf8c28ab9c8d69bec48
siam.kiw <siam13792@gmail.com>
2018-05-03 18:08:24 +0700

    Update attack monster

--- FallenSpace v1.0.0/Assets/Script/MonterScript.cs
+++ FallenSpace v1.0.0/Assets/Script/MonterScript.cs
@@ -7,9 +7,18 @@ public class MonterScript : MonoBehaviour {
 	string MontersURL = "http://www.bunlab.net/sharp/game/monters.php";
 	public float speedMonter = 0.001f;
 
+	public int curHealth;
+	public Animator anim;
+
+	void Awake(){
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		if(curHealth <= 0){
+			Debug.Log("dieee");
+		}
 	}
 	
 	// Update is called once per frame
@@ -20,7 +29,18 @@ public class MonterScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
-		Healthbar.health -= 100f/2;
+		Debug.Log (col.GetInstanceID());
+		if (col.GetInstanceID () == -50742) {
+			Healthbar.health -= 100f / 2;
+		} else if (col.GetInstanceID () == -64132) {
+			curHealth -= 5;
+			Debug.Log ("hit");
+			anim.SetTrigger ("hit");
+		} else if (curHealth <= 0) {
+			Debug.Log ("Monster Die");
+			anim.SetBool ("dead", true);
+		} 
+
 	}
 
 }
